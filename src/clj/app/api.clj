6593bb4038f5671d76a53a2755cfecfc38c16490
Daniel Neal <danielneal2@gmail.com>
2014-05-18 18:30:52 +0100

    Pretty much finished the material

--- src/clj/app/api.clj
+++ src/clj/app/api.clj
@@ -1,13 +1,24 @@
 (ns app.api
   (:require [tailrecursion.castra :refer [defrpc]]))
 
-
 (def options [{:title "core.async"
-               :description "Hoare's Communicating Sequential Processes - like go-lang"}
+               :description "asynchronous programming using channels"}
               {:title "core.logic"
-               :description "miniKanren logic programming - like a mini-Prolog"}
+               :description "embedded logic language like Prolog"}
               {:title "core.typed"
-               :description "Gradual typing for Clojure"}])
+               :description "gradual typing for clojure"}
+              {:title "datomic"
+               :description "immutable database with time support and datalog query language"}
+              {:title "instaparse"
+               :description "make grammar-based parsing as easy to use as regexes"}
+              {:title "clojure-sql and yesql"
+               :description "my two favourite database access libraries"}
+              {:title "hoplon"
+               :description "Clojure/ClojureScript web framework based on spreadsheet-like cells and HLisp"}
+              {:title "core.match"
+               :description "pattern matching as a library"}
+              {:title "overtone"
+               :description "clojure wrapper for supercollider (making noises)"}])
 
 (def all-votes (atom {}))
 
